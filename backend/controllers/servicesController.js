f8fa608f0cff36e80a8424dbdc9590e5637c9782
Shahriar Fahim <shahriarfahim19@gmail.com>
2021-10-23 21:52:06 +0600

    [Update] Service API Updated

--- backend/controllers/servicesController.js
+++ backend/controllers/servicesController.js
@@ -27,4 +27,64 @@ const createService = async (req, res) => {
   }
 };
 
-module.exports = { createService };
+const getService = async (req, res) => {
+  try {
+    const service = await Service.find();
+    res.status(200).json({
+      message: 'All Services',
+      result: service,
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+const getServiceById = async (req, res) => {
+  try {
+    const serviceById = await Service.findById({ _id: req.params.id });
+    res.status(200).json({
+      message: 'Service by ID',
+      result: serviceById,
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+const getLimitedService = async (req, res) => {
+  try {
+    const service = await Service.find()
+      .select({
+        desc1: 0,
+        desc2: 0,
+        desc3: 0,
+        desc4: 0,
+      })
+      .limit(4)
+      .exec();
+    res.status(200).json({
+      message: 'Landing Page Services',
+      result: service,
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+const deleteService = async (req, res) => {
+  try {
+    const service = await Service.findByIdAndDelete({ _id: req.params.id });
+    await cloudinary.uploader.destroy(service.cloudinary_id);
+    res.status(200).json({ message: 'Service Deleted Successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createService,
+  getService,
+  getServiceById,
+  getLimitedService,
+  deleteService,
+};
--- backend/routes/services.js
+++ backend/routes/services.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
 const uploader = require('../lib/multer');
-const { createService } = require('../controllers/servicesController');
+const {
+  createService,
+  getService,
+  getServiceById,
+  getLimitedService,
+  deleteService,
+} = require('../controllers/servicesController');
 
 router.post('/', uploader.single('image'), createService);
-router.get('/', async (req, res) => {
-  res.send('Hello');
-});
+router.get('/', getService);
+router.get('/limited', getLimitedService);
+router.get('/:id', getServiceById);
+router.delete('/:id', deleteService);
 
 module.exports = router;
