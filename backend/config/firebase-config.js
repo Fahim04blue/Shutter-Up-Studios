f35764267744bd7e13657ad229bb428df451dbcd
shahriar04ssl <shahriar.saleh@sslwireless.com>
2023-01-28 00:50:05 +0600

    removed fire base service

--- backend/config/firebase-config.js
+++ backend/config/firebase-config.js
@@ -1,9 +1,20 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./shutter-up-studio-serviceAccount.json');
+// const serviceAccount = require('./shutter-up-studio-serviceAccount.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: 'service_account',
+    project_id: 'shutter-up-studio',
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_CERT,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
+  }),
 });
 
 module.exports = admin;
